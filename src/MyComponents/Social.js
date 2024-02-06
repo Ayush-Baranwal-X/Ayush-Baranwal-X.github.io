d8590621d8224a4aee9807817268eb9a91c82f52
Ayush Kumar Baranwal <ayushabhilasha@gmail.com>
2024-02-06 18:32:40 +0530

    Fixes

--- src/MyComponents/Social.js
+++ src/MyComponents/Social.js
@@ -7,28 +7,52 @@ const Social = (props) => {
             <br></br>
             <br></br>
             <center>
-                <div style={{ margin: '0px 19.5%' }}>
-                    <Link to = 'mailto:ayushabhilasha@gmail.com' style={{color:props.darkMode===true?'white':'black'}}>
-                    <i class="fa-solid fa-envelope fa-4x" id={props.darkMode===true?'hover_black':'hover_white'}></i>
-                    </Link>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to = 'https://github.com/Ayush-Baranwal-X' target='blank' style={{color:props.darkMode===true?'white':'black'}}>
-                    <i class="fa-brands fa-github fa-4x" id={props.darkMode===true?'hover_black':'hover_white'}></i>
-                    </Link>
-                    {props.mobile === true?<><br></br><br></br></>:''}
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to = 'https://www.linkedin.com/in/akbiitd/' target='blank' style={{color:props.darkMode===true?'white':'black'}}>
-                    <i class="fa-brands fa-linkedin fa-4x" id={props.darkMode===true?'hover_black':'hover_white'}></i>
-                    </Link>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to = 'https://www.instagram.com/cybershot_x/' target='blank' style={{color:props.darkMode===true?'white':'black'}}>
-                    <i class="fa-brands fa-instagram fa-4x" id={props.darkMode===true?'hover_black':'hover_white'}></i>
-                    </Link>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to = 'https://calendly.com/akbiitd/' target='blank' style={{color:props.darkMode===true?'white':'black'}}>
-                    <i class="fa-solid fa-calendar fa-4x" id={props.darkMode===true?'hover_black':'hover_white'}></i>
-                    </Link>
-                </div>
+                {props.mobile === false ?
+                    <div style={{ margin: '0px 19.5%' }}>
+                        <Link to='mailto:ayushabhilasha@gmail.com' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-solid fa-envelope fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://github.com/Ayush-Baranwal-X' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-brands fa-github fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://www.linkedin.com/in/akbiitd/' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-brands fa-linkedin fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://www.instagram.com/cybershot_x/' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-brands fa-instagram fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://calendly.com/akbiitd/' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-solid fa-calendar fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                    </div>
+                    :
+                    <div style={{ margin: '0px 10%' }}>
+                        <Link to='mailto:ayushabhilasha@gmail.com' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-solid fa-envelope fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://github.com/Ayush-Baranwal-X' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-brands fa-github fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://www.linkedin.com/in/akbiitd/' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-brands fa-linkedin fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://www.instagram.com/cybershot_x/' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-brands fa-instagram fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to='https://calendly.com/akbiitd/' target='blank' style={{ color: props.darkMode === true ? 'white' : 'black' }}>
+                            <i class="fa-solid fa-calendar fa-4x" id={props.darkMode === true ? 'hover_black' : 'hover_white'}></i>
+                        </Link>
+                    </div>
+                }
+
             </center>
         </div>
     )
