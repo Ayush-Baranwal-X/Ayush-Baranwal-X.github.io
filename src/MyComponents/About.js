2e0ae71badd2bc3c2c8b6257d621be29133a8bc6
Ayush Kumar Baranwal <ayushabhilasha@gmail.com>
2024-02-21 19:49:14 +0530

    Added the section for the tech I am interested in

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -9,11 +9,33 @@ const About = (props) => {
                     {/* <h1 className='animate-character' style={{margin : '5px 0px',fontSize:'50px', textAlign:'center'}}>Ayush Kumar Baranwal</h1> */}
                     <img src='Ayush.jpg' style={{ float: 'right', width: '44%', height: '44%', marginLeft: "15px" }}></img>
                     <br></br>
-                    <p style={{ textAlign: 'justify', marginBottom: '25px', fontSize: '16px' }}>
+                    <p style={{ textAlign: 'justify', marginBottom: '25px', fontSize: '16.5px' }}>
                         Hi there! I'm Ayush Kumar Baranwal, a driven software enthusiast and Engineering graduate from the prestigious Indian Institute of Technology Delhi. My journey through academia and diverse internships has fueled my passion for software development and technology innovation. I thrive on transforming ideas into reality through code, crafting impactful solutions that blend creativity with technical excellence. My experience in analyzing market opportunities, developing user-centric applications like websites and Android apps, and crafting machine learning models has honed my skills across various tech stacks, including C++, Python, Java, and web development frameworks like React, Node.js and Django.
                     </p>
-                    <p style={{ textAlign: 'justify', fontSize: '16px' }}>
+                    <p style={{ textAlign: 'justify', fontSize: '16.5px' }}>
                         I'm on a quest to continuously learn, innovate, and collaborate on projects that push the boundaries of technology. Let's connect and explore opportunities where I can contribute my technical expertise and passion for software development! You can reach me at ayushkumarbaranwal1@gmail.com
+                    </p>
+                    <br></br>
+                    <h1 className='glow-on-hover' style={{ height: '10%', fontSize: '26px', margin: '0.5% 0px', width: '26%', paddingLeft: '1%' }}>I'm interested in...</h1>
+                    <p style={{ textAlign: 'justify', fontSize: '17px' }}>
+                        <table>
+                            <tr>
+                                <td><b>Web Development</b></td>
+                                <td>React, Django, Next.js, HTML, CSS, Typescript, JavaScript, Firebase, Pythonanywhere</td>
+                            </tr>
+                            <tr>
+                                <td><b>Game Development</b></td>
+                                <td>Unity Game Engine, C#, Godot Game Engine, GDScript</td>
+                            </tr>
+                            <tr>
+                                <td><b>Machine Learning</b></td>
+                                <td>PyTorch, NumPy, Pandas, Matplotlib, Scikit-learn</td>
+                            </tr>
+                            <tr>
+                                <td style={{ paddingRight: '15px' }}><b>Algorithmic Problem-Solving</b></td>
+                                <td>C, C++, Java, Python</td>
+                            </tr>
+                        </table>
                     </p>
                 </div>
                 :
@@ -26,7 +48,7 @@ const About = (props) => {
                     </center>
                     {/* <h1 className='animate-character' style={{margin : '5px 0px',fontSize:'50px', textAlign:'center'}}>Ayush Kumar Baranwal</h1> */}
                     <center>
-                        <img src='Ayush.jpg' style={{width: '44%', height: '44%', marginLeft: "15px" }}></img>
+                        <img src='Ayush.jpg' style={{ width: '44%', height: '44%', marginLeft: "15px" }}></img>
                     </center>
                     <br></br>
                     <p style={{ textAlign: 'justify', marginBottom: '25px', fontSize: '16px' }}>
@@ -35,6 +57,17 @@ const About = (props) => {
                     <p style={{ textAlign: 'justify', fontSize: '16px' }}>
                         I'm on a quest to continuously learn, innovate, and collaborate on projects that push the boundaries of technology. Let's connect and explore opportunities where I can contribute my technical expertise and passion for software development! You can reach me at ayushkumarbaranwal1@gmail.com
                     </p>
+                    <br></br>
+                    <h1 className='glow-on-hover' style={{ height: '10%', fontSize: '20px', margin: '0.5% 0px', width: '55%', paddingLeft: '1%' }}>I'm interested in...</h1>
+                    <p style={{ textAlign: 'justify', fontSize: '17px' }}>
+                        <b>Web Development</b>: React, Django, Next.js, HTML, CSS, Typescript, JavaScript, Bootstrap
+                        <br></br>
+                        <b>Game Development</b>: Unity Game Engine, C#, Godot Game Engine, GDScript
+                        <br></br>
+                        <b>Machine Learning</b>: PyTorch, NumPy, Pandas, Matplotlib, Scikit-learn
+                        <br></br>
+                        <b>Algorithmic Problem-Solving</b>: C, C++, Java, Python
+                    </p>
                 </div>
                 :
                 <></>
@@ -42,18 +75,29 @@ const About = (props) => {
             {props.mobile === true ?
                 <div style={{ margin: '0px 5%' }}>
                     <center>
-                        <h1 className='glow-on-hover' style={{ marginTop: '0.5%', width: '98%', textAlign: 'center', paddingTop: props.tab === false ? '1.5%' : '0.25%', marginBottom: '4%' }}>Ayush Kumar Baranwal</h1>
+                        <h1 className='glow-on-hover' style={{ marginTop: '0.5%', width: '98%', textAlign: 'center', paddingTop: '1.5%', marginBottom: '4%' }}>Ayush Kumar Baranwal</h1>
                     </center>
                     {/* <h1 className='animate-character' style={{margin : '5px 0px',fontSize:'50px', textAlign:'center'}}>Ayush Kumar Baranwal</h1> */}
                     <center>
-                        <img src='Ayush.jpg' style={{width: '50%', height:'50%', marginLeft: "15px" }}></img>
+                        <img src='Ayush.jpg' style={{ width: '50%', height: '50%', marginLeft: "15px" }}></img>
                     </center>
                     <br></br>
-                    <p style={{ textAlign: 'justify', marginBottom: '25px' , fontSize: '16px'}}>
+                    <p style={{ textAlign: 'justify', marginBottom: '25px', fontSize: '16px' }}>
                         Hi there! I'm Ayush Kumar Baranwal, a driven software enthusiast and Engineering graduate from the prestigious Indian Institute of Technology Delhi. My journey through academia and diverse internships has fueled my passion for software development and technology innovation. I thrive on transforming ideas into reality through code, crafting impactful solutions that blend creativity with technical excellence. My experience in analyzing market opportunities, developing user-centric applications like websites and Android apps, and crafting machine learning models has honed my skills across various tech stacks, including C++, Python, Java, and web development frameworks like React, Node.js and Django.
                     </p>
-                    <p style={{ textAlign: 'justify' , fontSize : '16px'}}>
+                    <p style={{ textAlign: 'justify', fontSize: '16px' }}>
                         I'm on a quest to continuously learn, innovate, and collaborate on projects that push the boundaries of technology. Let's connect and explore opportunities where I can contribute my technical expertise and passion for software development!
+                    </p>
+                    <br></br>
+                    <h1 className='glow-on-hover' style={{ height: '10%', fontSize: '20px', margin: '0.5% 0px', width: '55%', paddingLeft: '1%' }}>I'm interested in...</h1>
+                    <p style={{ textAlign: 'justify', fontSize: '17px' }}>
+                        <b>Web Development</b>: React, Django, Next.js, HTML, CSS, Typescript, JavaScript, Bootstrap
+                        <br></br>
+                        <b>Game Development</b>: Unity Game Engine, C#, Godot Game Engine, GDScript
+                        <br></br>
+                        <b>Machine Learning</b>: PyTorch, NumPy, Pandas, Matplotlib, Scikit-learn
+                        <br></br>
+                        <b>Algorithmic Problem-Solving</b>: C, C++, Java, Python
                     </p>
                 </div>
                 :
