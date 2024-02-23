2761056247f4bd94f3be89b74678220a7bb4fdff
Ayush Kumar Baranwal <ayushabhilasha@gmail.com>
2024-02-23 17:05:29 +0530

    fixes

--- src/MyComponents/PageNotFound.js
+++ src/MyComponents/PageNotFound.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Pagenotfound = () => {
   return (
-    <div style={{margin:'15%'}}>
+    <div style={{margin:'14.5%'}}>
         <center>    
-            Sorry but it appears that you are looking for an invalid page. You should just use the Navigation Bar to navigate.
+            Sorry, but it appears that you trying to use the url bar to access a path other than the main url (https://ayush-baranwal-x.github.io/).
+            <br></br>
+            You requsted to refrain from this and just use the navigation bar at the top of the website to navigate.
         </center>
     </div>
   )
